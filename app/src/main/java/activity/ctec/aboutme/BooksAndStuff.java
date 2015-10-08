5b9a55d75d0041527296ea8dd04facefed6c4c48
Jac0bAnderson <jake912235@gmail.com>
2015-10-07 21:59:15 -0600

    Book page added.
    the final about me page has been added with buttons, text, and picture
    visibility toggles.

---
+++ app/src/main/java/activity/ctec/aboutme/BooksAndStuff.java
@@ -0,0 +1,95 @@
+package activity.ctec.aboutme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class BooksAndStuff extends AppCompatActivity {
+
+    private Button homeButton;
+    private Button bookButton;
+    private TextView bookText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_and_stuff);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_books_and_stuff, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeVisibility() {
+        //Gametext visible settings
+
+        if (bookText.getVisibility() == View.VISIBLE) {
+            bookText.setVisibility(View.GONE);
+
+
+        } else {
+            bookText.setVisibility(View.VISIBLE);
+        }
+        //gamePic visible settings
+        if (bookPic.getVisibility() == View.VISIBLE) {
+            bookPic.setVisibility(View.GONE);
+
+
+        } else {
+            bookPic.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+
+
+
+    private void setUpListeners() {
+
+        book.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View GamesText) {
+                changeVisibility();
+            }
+        });
+
+
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View buttonView)
+            {
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+
+
+    }
+
+
+
+
+}
